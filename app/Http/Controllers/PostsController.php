efa396b35de5ac3cc4df35d8a8d8e0f8f4cac6a3
Paul <paul@Pauls-Macbook.local>
2018-03-28 22:03:02 -0500

    vid 11 - 12:23

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store()
+    {
+        dd(requsets()->all());
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,21 @@
 |
 */
 
-Route::get('/', function () {
-    return view('posts.index');
-});
+Route::get('/', 'PostsController@index');
+Route::get('/posts/create', 'PostsController@create');
+Route::post('/posts', 'PostsController@store');
+
+// Route::get('/posts/{post}', function ($id) {
+//     return view('posts.show');
+// });
+
+
+/*
+GET /posts
+GET /posts/create
+POST /posts
+GET /posts/{id}/edit
+GET /posts/{id}
+PATCH /posts/{id}
+DELETE /posts/{id}
+*/
